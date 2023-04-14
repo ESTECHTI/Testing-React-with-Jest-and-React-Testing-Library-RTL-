a8dc4b59791a029823c0d3368e1304564e8b5b57
ESTECHTI <e.s.tech@outlook.com>
2023-04-14 16:55:13 -0300

    OrderEntry and its test were created

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handler error response
+      });
+  }, [optionType]);
+
+  // TODO: replace ´null´ with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
